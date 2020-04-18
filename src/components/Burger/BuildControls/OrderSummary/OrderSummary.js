34628fba467b14bd1a95b456110e419a877ccd02
asachan2707 <asachan2707@gmail.com>
2020-04-18 20:59:53 +0530

    Added Order summary Modal

---
+++ src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Aux from '../../../../hoc/Auxilliry';
+
+const orderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(key => {
+            return (
+                <li>
+                    <span key={key} style={{ textTransform: 'capitalize' }}>{key}</span>: {props.ingredients[key]}
+                </li>
+            )
+        });
+        console.log('ingredientSummary:: ', ingredientSummary);
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    )
+}
+
+export default orderSummary;
