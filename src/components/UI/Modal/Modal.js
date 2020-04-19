a2c63153c5fcc090135d4bdeb22a0237494fd113
asachan2707 <asachan2707@gmail.com>
2020-04-20 02:45:48 +0530

    Inhancing the App performance
    Component which doesnot need to re-render because of no changes in props can be restrict from 'shouldComponentUpdate' lifecycle method.
    
    Lifecycle method works in class based component.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,31 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxilliry';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = ( props ) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProp, nextState) {
+        return nextProp.show !== this.props.show;
+    }
+
+    // if shouldComponentUpdate retruns FALSE then this colmponent won't re-render.
+    componentWillUpdate() { }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+export default Modal;
