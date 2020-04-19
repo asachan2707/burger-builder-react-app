3f4c1f3e73757936ca1ec2724dee93b62dc32fd3
asachan2707 <asachan2707@gmail.com>
2020-04-20 00:37:55 +0530

    Revert "Reused backdrop component with side drawer."
    This reverts commit d44281c39636feafd31f2c58e0fff84c3f0dcc23.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,14 +3,10 @@ import Aux from '../../hoc/Auxilliry';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-import Backdrop from '../UI/Backdrop/Backdrop';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
-        <div className={classes.DesktopOnly}>
-            <Backdrop show='true' clicked={() => { }} />
-        </div>
         <SideDrawer />
         <main className={classes.Content}>
             {props.children}
