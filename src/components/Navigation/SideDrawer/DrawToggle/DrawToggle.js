875127ee52549280f08abb53fce9c85d68a124de
asachan2707 <asachan2707@gmail.com>
2020-04-20 02:17:29 +0530

    Added DrawerToggle Icon through CSS

--- src/components/Navigation/SideDrawer/DrawToggle/DrawToggle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const drawToggle = props => (
-    <div onClick={props.clicked}>Menu</div>
-);
-
-export default drawToggle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import DrawToggle from '../SideDrawer/DrawToggle/DrawToggle';
+import DrawToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
     <header className={classes.Toolbar}>
